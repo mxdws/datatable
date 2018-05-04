488f109de4592d4e5fffdfaebd3c721eac61cc32
mxdws <26325269+mxdws@users.noreply.github.com>
2018-05-04 22:38:53 +0100

    Adding the repeater function
    The repeater function is a recursive function that asynchronously gets JSON and sticks it in a file. 
    
    There are probably better ways of doing it, but I think this works.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var fs          = require('fs');
 var http        = require('http');
 var gravatar    = require('gravatar');
 var runSequence = require('run-sequence');
+var request     = require('request');
 
 // load environment variables
 require('dotenv').config()
@@ -51,42 +52,56 @@ gulp.task('getData', function() {
 
   console.log("Getting football data from api");
 
-  var token = process.env.TOKEN;
+  // Send out a call for Repeater, which is a recursive callback
+  // function to populate data tables one by one
+  // That solution came from this article
+  // http://www.richardrodger.com/2011/04/21/node-js-how-to-write-a-for-loop-with-callbacks/#.WuzE6y-ZPVr
 
-  var options = {
-    'method': 'GET',
-    'hostname': 'api.football-data.org',
-    'path': '/v1/soccerseasons/445/fixtures',
-    'headers': {
-      'X-Auth-Token': token
-    }
-  };
-
-  http.get(options, function(res) {
-    var body = '';
-    res.on('data', function(chunk) {
-      body += chunk;
-    });
-    res.on('end', function() {
-
-      // Try and write the result to a data file
-
-      fs.writeFile('./_data/epl201718fix.json', body, function(err) {
-        if(err) {
-          console.log(err);
-        } else {
-          console.log("Comments data saved.");
-        }
-      });
-
-    });
-  //})
-  //.on('error', function(e) {
-  //  console.log("Got error: ", e);
-  });
-
+  repeater(0)
 });
 
+function repeater(counter) {
+  var metrics = ["fixtures","leagueTable","teams"];
+  var datasuffix = ["fixtures","standings","teams"];
+  var token = process.env.TOKEN;
+  var competitions = ['445','446','447','448','450','451','452','453','455','456','459','464'];
+  var leaguecodes= ['epl','elc','el1','el2','fl1','fl2','bl1','bl2','pd','sa','sb','cl'];
+  var season = '201718';
+
+  if ( counter < ( competitions.length * metrics.length )) {
+    var a = Math.floor(counter / 3);
+    var b = counter % 3;
+    console.log("Competition: " + a + ", Metric: " + b)
+    console.log("Getting competition " + competitions[a] + metrics[b] + " data from api");
+
+    var options = {
+      'url': 'https://api.football-data.org/v1/competitions/' + competitions[a] + '/' + metrics[b],
+      'headers': {
+        'X-Auth-Token': token
+      }
+    };
+
+    request(options, function(err, response, body){
+      if(!err && response.statusCode === 200){
+        //var body = JSON.parse(body);
+        //var comments = {};
+        // write our data to a file where our site generator can get it.
+        fs.writeFile('./_data/' + leaguecodes[a] + '-' + season + '-' + datasuffix[b] + '.json', body, function(err) {
+          if(err) {
+            console.log(err);
+          } else {
+            console.log('External data saved - ' + leaguecodes[a] + '-' + season + '-' + datasuffix[b] + '.json');
+            repeater( counter + 1 );
+          }
+        });
+
+      } else {
+        console.log(err);
+        console.log("Couldn't get data :-(");
+      }
+    });
+  }
+}
 
 
 // Build and optimise the site and serve it locally.
